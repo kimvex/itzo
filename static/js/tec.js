48c93935bfb5abab237d1fd4afa6cc0c5959f6df
Benjamin DeLaCruz <chomi.numb.94@gmail.com>
2015-01-28 15:25:29 -0600

    Creamos el html, tambien creamos los css, agregamos los icon-fonts, agregamos el js que usa Jquery

---
+++ static/js/tec.js
@@ -0,0 +1,22 @@
+var $mostrarMUno = $('#u'),
+	$icoHi = $('#icoHidden');
+function inicio(){
+	var icoInicio = document.getElementById('inicioIco'),
+		icoH = document.getElementById('icoHidden');;
+
+	icoInicio.title = "Inicio";
+	icoH.title = "Dame click";
+
+	$('#icoHidden').hover(iconoHidden);
+	//$('#icoHidden').on('click',menuOcultoIco);
+}
+function iconoHidden(){
+	$('.uIco').toggleClass('icon-graduate');
+}
+function menuOcultoIco(b){
+	b.preventDefault();
+	b.stopPropagation(); 
+	$mostrarMUno.slideToggle();
+}
+
+$icoHi.click(menuOcultoIco);
