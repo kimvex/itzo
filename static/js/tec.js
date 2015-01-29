eb9e376cd6b59c6f6bffd48a4ea01a07f7fa3ad3
Benjamin DeLaCruz <chomi.numb.94@gmail.com>
2015-01-29 11:03:12 -0600

    Cambiamos los nombres de las clases, agregamos mas submenus, en el js hicimos que los menus aparezcan y desaparezcan con respecto a donde le den click, creamos el diseño responsivo patr

--- static/js/tec.js
+++ static/js/tec.js
@@ -1,22 +1,93 @@
 var $mostrarMUno = $('#menuOcultoNID'),
-	$icoHi = $('#icoHidden');
+	$mostrarMDos = $('#menuOcultoNID2'),
+	$mostrarMTres = $('#menuOcultoNID3'),
+	$icoHi = $('#menuItem-instituto'),
+	$icoOF = $('#icoHiddenOE'),
+	$cs    = $('#menuItem-centroServicio');
+var ni = false, oe = false, cds = false;
 function inicio(){
 	var icoInicio = document.getElementById('inicioIco'),
-		icoH = document.getElementById('icoHidden');;
+		icoH = document.getElementById('menuItem-instituto'),
+		icoB = document.getElementById('icoBuzon');
 
 	icoInicio.title = "Inicio";
 	icoH.title = "Dame click";
+	icoBuzon.title = "BUZÓN DE SUGERENCIAS";
 
-	$('#icoHidden').hover(iconoHidden);
-	//$('#icoHidden').on('click',menuOcultoIco);
+	$('#menuItem-instituto').hover(iconoHidden);
+	//$('#menuItem-instituto').on('click',menuOcultoIco);
 }
 function iconoHidden(){
 	$('.uIco').toggleClass('icon-graduate');
 }
 function menuOcultoIco(b){
+	if(oe == true){
+		$mostrarMDos.slideToggle();
+		oe = false;
+	}
+	else if(cds == true){
+		$mostrarMTres.slideToggle();
+		cds = false;
+	}
 	b.preventDefault();
 	b.stopPropagation(); 
 	$mostrarMUno.slideToggle();
+	if(ni == false){
+	ni = true;
+	}else if(ni == true){
+		ni = false;
+	}
+	//console.log(oe);
+	//console.log(ni);
+	//console.log(cds);
+}
+function menuOcultoIco2(b){
+	if(ni == true){
+		$mostrarMUno.slideToggle();
+		ni = false;
+	}
+	else if(cds == true){
+		$mostrarMTres.slideToggle();
+		cds = false;
+	}
+	b.preventDefault();
+	b.stopPropagation(); 
+	$mostrarMDos.slideToggle();
+	if(oe == false){
+	oe = true;
+	}
+	else
+	{
+		oe = false;
+	}
+	//console.log(oe);
+	//console.log(ni);
+	//console.log(cds);
+}
+function menuOcultoIco3(b){
+	if(ni == true){
+		$mostrarMUno.slideToggle();
+		ni = false;
+	}
+	else if(oe == true){
+		$mostrarMDos.slideToggle();
+		oe = false;
+	}
+	b.preventDefault();
+	b.stopPropagation(); 
+	$mostrarMTres.slideToggle();
+	if(cds == false){
+	cds = true;
+	}
+	else
+	{
+		cds = false;
+	}
+	//console.log(oe);
+	//console.log(ni);
+	//console.log(cds);
 }
 
 $icoHi.click(menuOcultoIco);
+$icoOF.click(menuOcultoIco2);
+$cs.click(menuOcultoIco3);
